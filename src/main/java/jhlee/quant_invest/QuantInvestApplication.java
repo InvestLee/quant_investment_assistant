bd5c85736a699b9177d543da5a27e5004b1b86cc
InvestLee <dlwlgud216@nate.com>
2024-05-01 19:41:28 +0900

    프로젝트 생성

---
+++ src/main/java/jhlee/quant_invest/QuantInvestApplication.java
@@ -0,0 +1,13 @@
+package jhlee.quant_invest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantInvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantInvestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jhlee/quant_invest/QuantInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package jhlee.quant_invest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantInvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
