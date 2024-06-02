03b686fe12cf4b95253857f18d587d75c120d37f
InvestLee <dlwlgud216@nate.com>
2024-06-02 19:14:56 +0900

    VUE 및 스프링부트 연동

---
+++ api/src/main/java/jhlee/api/domain/controller/HomeController.java
@@ -0,0 +1,13 @@
+package jhlee.api.domain.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
--- front/quant_invest/vue.config.js
+++ front/quant_invest/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: 'C:/study/quant_invest/api/src/main/resources/static', // Build Directory
+  devServer: {
+      proxy: 'http://localhost:8080' // Spring Boot Server
+  }
 })
